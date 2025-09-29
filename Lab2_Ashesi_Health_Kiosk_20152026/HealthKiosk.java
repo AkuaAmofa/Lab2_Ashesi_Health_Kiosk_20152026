5d313c63ab88a20c5679d1274f3aed64c4223d1c
Akua Amofa <akua.amofa@ashesi.edu.gh>
2025-09-29 21:26:10 +0000

    Final work

--- Lab2_Ashesi_Health_Kiosk_20152026/HealthKiosk.java
+++ Lab2_Ashesi_Health_Kiosk_20152026/HealthKiosk.java
@@ -35,41 +35,43 @@ public class HealthKiosk {
                 System.out.println("Invalid service code");
         }
 
-
-        //Task 2
+        // Task 2
         double bmi = 0;
         double finalOptionValue = 0;
 
         if (serviceCode == 'T') {
-            System.out.print("Kindly enter 1 for BMI, 2 for Dosage, 3 for Trig helper ");
+            System.out.print("Kindly enter 1 for BMI, 2 for Dosage, 3 for Trig helper: ");
             int option = input.nextInt();
 
-            if (option == 1){
+            if (option == 1) {
+                // BMI
                 System.out.print("Kindly enter your weight in kg: ");
                 double weight = input.nextDouble();
                 System.out.print("Kindly enter your height in meters: ");
                 double height = input.nextDouble();
 
-                bmi = weight / (height*height);
+                bmi = weight / (height * height);
                 double finalBmi = Math.round(bmi * 10) / 10.0;
 
-                System.out.print("BMI: " + finalBmi + "Weight Category: ");
-                if (finalBmi < 18.5) System.out.print("Underweight");
-                else if (finalBmi < 25) System.out.print("Normal");
-                else if (finalBmi < 30) System.out.print("Overweight");
+                System.out.print("BMI: " + finalBmi + "  Weight Category: ");
+                if (finalBmi < 18.5) System.out.println("Underweight");
+                else if (finalBmi < 25) System.out.println("Normal");
+                else if (finalBmi < 30) System.out.println("Overweight");
                 else System.out.println("Obese");
 
                 finalOptionValue = Math.round(bmi);
 
-            }else if (option == 2){
+            } else if (option == 2) {
+                // Dosage
                 System.out.print("Kindly enter the dosage required in mg: ");
                 double dosage = input.nextDouble();
                 int tablets = (int) Math.ceil(dosage / 250.0);
                 System.out.println("Tablets Number: " + tablets);
 
                 finalOptionValue = tablets;
-        
-            }else if (option == 3){
+
+            } else if (option == 3) {
+                // Trig
                 System.out.print("Kindly enter an angle in degrees: ");
                 double angle = input.nextDouble();
                 double radians = Math.toRadians(angle);
@@ -80,20 +82,16 @@ public class HealthKiosk {
                 System.out.println("sin = " + sinValue + ", cos = " + cosValue);
 
                 finalOptionValue = Math.round(Math.sin(radians) * 100);
-
             }
-            
-
         }
 
-        //Task 3
+        // Task 3
         char randomLetter = (char)('A' + rand.nextInt(26));
         String digits = "";
-        for (int i = 0; i < 4; i++){
-            int a = 3 +rand.nextInt(7);
+        for (int i = 0; i < 4; i++) {
+            int a = 3 + rand.nextInt(7);
             digits = digits + a;
         }
-
         String studentID = randomLetter + digits;
 
         if (studentID.length() == 5 &&
@@ -102,40 +100,30 @@ public class HealthKiosk {
             Character.isDigit(studentID.charAt(2)) &&
             Character.isDigit(studentID.charAt(3)) &&
             Character.isDigit(studentID.charAt(4))) {
-            System.out.println("ID OK");
+            System.out.println("ID OK: " + studentID);
         } else {
             System.out.println("Invalid ID");
         }
 
-        //Task 4
+        // Task 4
         System.out.print("Kindly enter your first name: ");
         String name = input.next();
 
-        char base = Character.toUpperCase(name.charAt(0)); 
-        char shifted = (char) ('A' + (base - 'A' + 2) % 26); 
+        char base = Character.toUpperCase(name.charAt(0));
+        char shifted = (char) ('A' + (base - 'A' + 2) % 26);
 
-        String lastTwo = studentID.substring(3); 
+        String lastTwo = studentID.substring(3);
         String displayCode = shifted + lastTwo + "-" + (int) finalOptionValue;
 
         System.out.println("Display Code: " + displayCode);
 
-        //Task 5
+        // Task 5
         if (serviceCode == 'T') {
             double roundedBmi = Math.round(bmi * 10) / 10.0;
-            System.out.println(serviceType.toUpperCase() +
-                               " | ID=" + studentID +
-                               " | BMI=" + roundedBmi +
-                               " | Code=" + displayCode);
+            System.out.println(serviceType.toUpperCase() + "  ID=" + studentID + "  BMI=" + roundedBmi + "  Code=" + displayCode);
         } else {
-            System.out.println(serviceType.toUpperCase() +
-                               " | ID=" + studentID +
-                               " | Code=" + displayCode);
+            System.out.println(serviceType.toUpperCase() + "  ID=" + studentID + "  Code=" + displayCode);
         }
-
-
-
-
-
 
         input.close();
     }
